b7ad40f8a547e5a6f07dd64f7efc30a53a14f840
Cong Ngo <congngo@yahoo.com>
2023-01-28 20:08:11 -0800

    initial add

---
+++ gp_result_date.ps1
@@ -0,0 +1,5 @@
+﻿# must run as admin
+$user = $env:USERNAME
+$file = "$env:USERPROFILE\Desktop\GPRESULT_$(get-date -f MM-dd-yyyy).html"
+
+Get-GPResultantSetOfPolicy -user $user -Computer WIN7 -ReportType Html -Path $file
---
+++ sample_xml_singleNode.ps1
@@ -0,0 +1,27 @@
+﻿$c = [xml]'<?xml version="1.0" encoding="utf-16"?>
+<root>
+  <User_Blob>
+    <Item>
+      <Key>LogonMethod</Key>
+      <Value>prompt</Value>
+    </Item>
+    <Item>
+      <Key>ServerURLEntered</Key>
+      <Value>http://myserver/config.xml</Value>
+    </Item>
+    <Item>
+      <Key>ServerURLListUsers</Key>
+      <Value>
+        <LSOption>http://myurl/config.xml</LSOption>
+        <LSOption>http://myurl</LSOption>
+      </Value>
+    </Item>
+    <Item>
+      <Key>UserDisplayDimensions</Key>
+      <Value>fullscreen</Value>
+    </Item>
+  </User_Blob></root>'
+($c | Select-Xml -XPath "//Item[Key = 'LogonMethod']").Node.Value
+
+[xml]$xml = $c
+$xml.SelectSingleNode("//*[Key='LogonMethod']").Value
